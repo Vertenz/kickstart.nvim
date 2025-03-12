39809460e141d17722ec28b54284c1d9ed6985a8
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-03-12 17:24:44 -0400

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -182,6 +182,10 @@ vim.opt.spell = false
 vim.opt.spelllang = { 'en', 'en_us', 'ru', 'ru_ru' }
 
 vim.g.coc_global_extensions = { 'coc-spell-checker' }
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
